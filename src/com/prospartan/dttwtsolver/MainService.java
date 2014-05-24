bfe4f1e09b45e51d78527c2abf94c1047a2a895d
ProSpartan <happyreaper55@gmail.com>
2014-05-23 21:59:05 -0500

    Update MainService.java
    Changed from an ImageView to a Switch to give a better user interface when operating outside of the main activity.

--- src/com/prospartan/dttwtsolver/MainService.java
+++ src/com/prospartan/dttwtsolver/MainService.java
@@ -1,23 +1,20 @@
 package com.prospartan.dttwtsolver;
 
 import com.prospartan.dttwtsolver.DrawView;
-import com.prospartan.dttwtsolver.R;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
 
 public class MainService extends Service
 {
   private WindowManager winManager;
-  private ImageView imgView;
-  private TextView txtView;
   private DrawView drawView;
   
   @Override
@@ -32,20 +29,20 @@ public class MainService extends Service
 		super.onCreate();
     
 		winManager = (WindowManager)getSystemService(WINDOW_SERVICE);
-		imgView = new ImageView(this);
-		txtView = new TextView(this);
+		switchProgramOn = new Switch(this);
+		switchGameOn = new Switch(this);
 		drawView = new DrawView(this);
-    
-		txtView.setText("Off");
-		imgView.setImageResource(R.drawable.floating_icon);
+		
+		switchProgramOn.setChecked(false);
+		switchGameOn.setChecked(false);
 
-		final WindowManager.LayoutParams paramsImage = new WindowManager.LayoutParams(
+		final WindowManager.LayoutParams paramsSwitch = new WindowManager.LayoutParams(
 			WindowManager.LayoutParams.WRAP_CONTENT,
 			WindowManager.LayoutParams.WRAP_CONTENT,
 			WindowManager.LayoutParams.TYPE_PHONE,
 			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
 			PixelFormat.TRANSLUCENT);
-		final WindowManager.LayoutParams paramsText = new WindowManager.LayoutParams(
+		final WindowManager.LayoutParams paramsSwitchRunGame = new WindowManager.LayoutParams(
 			WindowManager.LayoutParams.WRAP_CONTENT, 
 			WindowManager.LayoutParams.WRAP_CONTENT, 
 			WindowManager.LayoutParams.TYPE_PHONE, 
@@ -58,65 +55,47 @@ public class MainService extends Service
 			WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, 
 			PixelFormat.TRANSLUCENT);
 		
-		paramsImage.gravity = Gravity.TOP | Gravity.LEFT;
+		paramsSwitch.gravity = Gravity.TOP | Gravity.LEFT;
 		paramsImage.x=0;
-		paramsImage.y=100;
+		paramsImage.y=200;
     
-		paramsText.gravity = Gravity.TOP | Gravity.LEFT;
-		paramsText.x=100;
-		paramsText.y=100;
+		paramsSwitchRunGame.gravity = Gravity.TOP | Gravity.Right;
+		paramsText.x=0;
+		paramsText.y=200;
     
 		paramsDraw.gravity = Gravity.TOP | Gravity.LEFT;
 		paramsDraw.x=0;
 		paramsDraw.y=0;
     
-		winManager.addView(imgView, paramsImage);
-		winManager.addView(txtView, paramsText);
-    
-		try{
-			imgView.setOnTouchListener(new View.OnTouchListener() {
-				private WindowManager.LayoutParams paramsF = paramsImage;
-				private int initX;
-				private int initY;
-				private float initTouchX;
-				private float initTouchY;
-			
-				@Override
-				public boolean onTouch(View v, MotionEvent event) 
-				{
-					switch(event.getAction()){
-					case MotionEvent.ACTION_DOWN:
-						initX = paramsF.x;
-						initY = paramsF.y;
-						initTouchX = event.getRawX();
-						initTouchY = event.getRawY();
-						break;
-					case MotionEvent.ACTION_UP:
-						break;
-					case MotionEvent.ACTION_MOVE:
-						paramsF.x = initX + (int) (event.getRawX() - initTouchX);
-						paramsF.y = initY + (int) (event.getRawY() - initTouchY);
-						winManager.updateViewLayout(imgView,  paramsF);
-						break;
-					}
-					return false;
-				}
-			});
-		} catch (Exception e){
-			//empty
-		}
+		winManager.addView(switchProgramOn, paramsSwitch);
 		
 		try{
-			imgView.setOnClickListener(new View.OnClickListener() {
+			switchProgramOn.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 				
 				@Override
-				public void onClick(View v) {
-					if(txtView.getText()=="Off"){
-						txtView.setText("On");
+				public void onCheckedChanged(CompoundButton compBtn, boolean isChecked) {
+					if(switchProgramOn.isChecked()){
+						winManager.addView(switchGameOn, paramsSwitchRunGame);
 						winManager.addView(drawView, paramsDraw);
+						try{
+							switchGameOn.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+								public void onCheckedChanged(CompoundButton compBtn, boolean isChecked)
+								{
+									if(switchGameOn.isChecked())
+									{
+										Toast.makeText(getApplicationContext(), "Switch is checked", Toast.LENGTH_LONG).show();
+									} else {
+										Toast.makeText(getApplicationContext(), "Switch is unchecked", Toast.LENGTH_LONG).show();
+									}
+								}
+							});
+						} catch (Exception e) {
+							//empty
+						}
 					} else {
-						txtView.setText("Off");
+						winManager.removeView(switchGameOn);
 						winManager.removeView(drawView);
+						switchGameOn.setChecked(false);
 					}
 				}
 			});
@@ -127,9 +106,14 @@ public class MainService extends Service
   
   @Override
 	public void onDestroy(){
+		if(switchProgramOn.isChecked())
+		{
+			winManager.removeView(switchGameOn);
+			winManager.removeView(drawView);
+			winManager.removeView(switchProgramOn);
+		} else {
+			winManager.removeView(switchProgramOn);
+		}
 		super.onDestroy();
-		if(imgView != null) winManager.removeView(imgView);
-		if(txtView != null) winManager.removeView(txtView);
-		if(txtView.getText()=="On") winManager.removeView(drawView);
 	}
 }
